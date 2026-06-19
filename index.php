ec31de5220d60ddae0945aeecc3481a29c90c6f1
Lawanyashiva <0401lawanya@gmail.com>
2026-06-19 11:22:23 +0530

    index file updated

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+// Laravel root entry point when the project root is served directly.
+// This forwards requests to the public Laravel front controller.
+
+require __DIR__ . '/public/index.php';
